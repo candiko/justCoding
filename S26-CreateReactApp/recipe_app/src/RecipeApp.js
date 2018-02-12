fee1691a12764c5a72ac499962f71d5b9871479b
DarisCalinor <candogan.tuzcu@gmail.com>
2018-02-12 17:21:15 +1100

    Added state to Recipe App. Getting used to React. Edited space indentation. Better code view.

--- S26-CreateReactApp/recipe_app/src/RecipeApp.js
+++ S26-CreateReactApp/recipe_app/src/RecipeApp.js
@@ -3,14 +3,51 @@ import RecipeList from './RecipeList';
 import Navbar from './Navbar';
 
 class RecipeApp extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipes: [
+        {
+          id: 1,
+          title: 'Fried Cauliflowers',
+          ingredients: ['1 head cauliflower, broken into small florets',
+            '1 cup cracker crumbs',
+            '1 egg'
+          ],
+          instructions: 'Place cauliflower into a pan of lightly salted boiling water. Cook for 5 to 8 minutes',
+          img: 'img/fried_cauliflower.jpg'
+        },
+        {
+          id: 2,
+          title: 'Hamburger',
+          ingredients: ['2 pounds ground beef',
+            '2 tablespoons Worcestershire sauce',
+            '1 egg, beaten'
+          ],
+          instructions: 'Preheat grill for high heat. In a large bowl, mix the ground beef, egg, bread crumbs, evaporated milk, Worcestershire sauce, cayenne pepper, and garlic using your hands. Form the mixture into 8 hamburger patties.',
+          img: 'img/hamburger.jpg'
+        },
+        {
+          id: 3,
+          title: 'Noodles',
+          ingredients: ['7 ounces steam fried Chinese noodles',
+            '6 slices ginger',
+            '2 stalks green onions(cut in 1 - inch strips)'
+          ],
+          instructions: 'Put noodles in 4 cups of salted boiling water. Cook 1-2 minutes. Stir occasionally.',
+          img: 'img/noodles.jpg'
+        },
+      ],
+      nextRecipeId: 3,
+    }
+  }
 
   render() {
     return (
       <div className="recipe-app">
         <Navbar />
         <div className="recipe-cards">
-          <RecipeList />          
+          <RecipeList recipes={this.state.recipes} />          
         </div>        
       </div>
     );
--- S26-CreateReactApp/recipe_app/src/RecipeCard.js
+++ S26-CreateReactApp/recipe_app/src/RecipeCard.js
@@ -3,34 +3,34 @@ import './RecipeCard.css';
 import PropTypes from 'prop-types';
 
 class RecipeCard extends Component {
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        ingredients: PropTypes.arrayOf(PropTypes.string).isRequired,
-        instructions: PropTypes.string.isRequired,
-        img: PropTypes.string.isRequired
-    }
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    ingredients: PropTypes.arrayOf(PropTypes.string).isRequired,
+    instructions: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired
+  }
     
-    render() {
-        return(
-            <div className="recipe-card">
-                <img
-                    src={this.props.img}
-                    alt={this.props.title}
-                />
-                <h2>{this.props.title}</h2>
-                <h4>Ingredients</h4>
-                <ul>
-                    {this.props.ingredients.map((i, index) => (
-                        <li>{i}</li>
-                    ))}
-                </ul>
-                <h4>Directions</h4>
-                <ul>
-                    <li>{this.props.instructions}</li>
-                </ul>
-            </div>
-        );
-    }
+  render() {
+    return(
+      <div className="recipe-card">
+        <img
+          src={this.props.img}
+          alt={this.props.title}
+        />
+        <h2>{this.props.title}</h2>
+        <h4>Ingredients</h4>
+        <ul>
+          {this.props.ingredients.map((i, index) => (
+            <li key={index}>{i}</li>
+          ))}
+        </ul>
+        <h4>Directions</h4>
+        <ul>
+          <li>{this.props.instructions}</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default RecipeCard;
